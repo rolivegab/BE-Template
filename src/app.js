35c067f11c820313c8aed4cbbe80ba9b89ca1a5e
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2021-04-06 12:36:23 -0300

    feat: implement balances router

--- src/app.js
+++ src/app.js
@@ -4,4 +4,6 @@ import { doAssociations } from "./service/doAssociations";
 
 doAssociations();
 export const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 app.use("/", routes);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getProfile } from "../middleware/getProfile";
+import { balancesRoute } from "./balances";
 import { contractsRoute } from "./contracts";
 import { jobsRoute } from "./jobs";
 
 export const routes = Router("/")
   .use("/contracts", getProfile, contractsRoute)
-  .use('/jobs', getProfile, jobsRoute);
+  .use('/jobs', getProfile, jobsRoute)
+  .use('/balances', getProfile, balancesRoute);
