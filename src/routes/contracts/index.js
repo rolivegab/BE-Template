61889cb0280e96b5ca03fa7e7c96c95baddb9487
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2021-04-06 11:31:41 -0300

    chore: move getProfile middleware to outside route

--- src/routes/contracts/index.js
+++ src/routes/contracts/index.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { Op } from "sequelize";
-import { getProfile } from "../../middleware/getProfile";
 import { Contract } from "../../models/Contract";
 
 export const contractsRoute = Router("/")
-  .get("/:id", getProfile, async (req, res) => {
+  .get("/:id", async (req, res) => {
     const { id } = req.params;
     const { id: profileId } = req.profile;
     const contract = await Contract.findOne({
@@ -16,7 +15,7 @@ export const contractsRoute = Router("/")
     if (!contract) return res.status(404).end();
     res.json(contract);
   })
-  .get("/", getProfile, async (req, res) => {
+  .get("/", async (req, res) => {
     const { id: profileId } = req.profile;
     const contracts = await Contract.findAll({
       where: {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,8 @@
 import { Router } from "express";
+import { getProfile } from "../middleware/getProfile";
 import { contractsRoute } from "./contracts";
+import { jobsRoute } from "./jobs";
 
-export const routes = Router("/").use("/contracts", contractsRoute);
+export const routes = Router("/")
+  .use("/contracts", getProfile, contractsRoute)
+  .use('/jobs', getProfile, jobsRoute);
