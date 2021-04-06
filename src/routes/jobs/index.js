bd68221e2b09688873fa1afcbca6193b7580faaa
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2021-04-06 12:07:45 -0300

    feat: implement jobs route

--- src/routes/jobs/index.js
+++ src/routes/jobs/index.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { Op } from "sequelize";
+import { Op, Transaction } from "sequelize";
 import { Contract } from "../../models/Contract";
 import { Job } from "../../models/Job";
+import { sequelize } from "../../service/sequelize";
 
 export const jobsRoute = Router("/")
   .get("/unpaid", async (req, res) => {
@@ -20,4 +21,38 @@ export const jobsRoute = Router("/")
     });
     const jobs = contracts.flatMap((i) => i.Jobs);
     res.json(jobs);
+  })
+  .post("/:jobId/pay", async (req, res) => {
+    const {jobId} = req.params
+    const profile = req.profile;
+    const job = await Job.findOne({
+      id: jobId,
+      paid: false,
+      include: {
+        model: Contract,
+        where: {
+          [Op.or]: [{ ClientId: profile.id }, { ContractorId: profile.id }],
+          status: "in_progress",
+        },
+        required: true,
+      }
+    });
+    await sequelize.transaction({isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE}, async (transaction) => {
+      if (profile.balance >= job.price) {
+        const paidJob = await Job.update({
+          paid: true,
+        }, {
+          where: {
+            id: jobId
+          },
+          transaction,
+        })
+        profile.balance = profile.balance - job.price;
+        await profile.save({transaction});
+  
+        res.json(true);
+      } else {
+        res.status(400).send('insufficient_balance')
+      }
+    });
   });
